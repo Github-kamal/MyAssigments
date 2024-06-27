dc34061853a0be8368ce10b62ba4af30f9d09f30
Github-kamal <128229652+Github-kamal@users.noreply.github.com>
2024-06-27 23:00:33 +0530

    Add files via upload
    Day 1 Home Assignment <2> Understanding Data Types and Variables in Java

---
+++ Chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+// Class Chrome created 
+public class Chrome {
+
+	public static void main(String[] args) {
+		// Declare and initialize variables
+		float version = 91.0f;
+		String developer ="Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		//print the variable using Syso
+		
+		System.out.println("Chrome Version :"+version);
+		System.out.println("Chrome developer :"+developer);
+		System.out.println("Chrome is Beta: "+isBeta);
+		System.out.println("Chrome Release Year :"+releaseYear);
+		System.out.println("Chrome shortcutKey:"+shortcutKey);
+		
+
+	}
+
+}
